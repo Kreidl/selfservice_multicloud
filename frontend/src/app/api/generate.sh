01b28c408ab571adcc74835b68464180a8c18792
Kreidl <lukaskienast0@gmail.com>
2020-02-09 08:57:19 +0100

    Corrected API URl in AWS VM

--- frontend/src/app/api/generate.sh
+++ frontend/src/app/api/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #Install api from backend (aws-vm service)
-openapi-generator generate -g typescript-angular -o . -i http://localhost:32400/api/openapi.json
+openapi-generator generate -g typescript-angular -o . -i http://localhost:32400/awsapi/openapi.json
