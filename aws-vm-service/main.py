70c248ca9c992683653eac8b4f26f26854001a7f
Kreidl <lukaskienast0@gmail.com>
2020-02-09 11:35:20 +0100

    Switched to LoadBalancer in Backend

--- aws-vm-service/main.py
+++ aws-vm-service/main.py
@@ -1,5 +1,4 @@
 import connexion
-from flask_cors import CORS
 
 # Create the application instance
 app = connexion.App(__name__, specification_dir='./')
@@ -8,9 +7,6 @@ port = 8080
 
 # Read the swagger.yml file to configure the endpoints
 app.add_api('swagger.yaml')
-
-# add CORS support
-CORS(app.app)
 
 #Default Route
 @app.route('/', methods=['GET'])
