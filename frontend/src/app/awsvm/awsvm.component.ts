5263ba591b04b9a523a247784d710497413f5523
Kreidl <lukaskienast0@gmail.com>
2020-02-12 20:17:43 +0100

    Implemented InstanceType Seach

--- frontend/src/app/awsvm/awsvm.component.ts
+++ frontend/src/app/awsvm/awsvm.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import { ImageService, ImageRequest } from '../api/aws/index';
+import { ImageService, ImageRequest, InstancetypeService } from '../api/aws/index';
 import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-awsvm',
   templateUrl: './awsvm.component.html',
   styleUrls: ['./awsvm.component.css'],
-  providers: [ImageService]
+  providers: [ImageService, InstancetypeService]
 })
 export class AwsvmComponent implements OnInit {
 
@@ -15,20 +15,27 @@ export class AwsvmComponent implements OnInit {
   selectedImagetype = '1';
   imageTypeDescription = "ImageId Input";
   images: any[];
-  displayNum = 10;
+  types: any[];
+  displayNumImages = 10;
+  displayNumTypes = 10;
 
   imagesSearchInfo = false;
+  typesSearchInfo = false;
 
   constructor(private imageService: ImageService,
+              private instancetypeService: InstancetypeService,
               private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.newVMForm = this.formBuilder.group({
-      image: [''],
-      images: ['']
+      imageInput: [''],
+      selectedImage: [''],
+      selectedtype: [''],
+      maxMemory: ['']
 
     });
     this.imagesSearchInfo = false;
+    this.typesSearchInfo = false;
   }
 
   onChange(event) {
@@ -46,7 +53,7 @@ export class AwsvmComponent implements OnInit {
     let imageRequest:ImageRequest = {};
 
 
-    if (this.f.image.value.length > 0){
+    if (this.f.imageInput.value.length > 0){
       //Check if it is imageId or imageName and set attribute accordingly
       if(this.selectedImagetype === '1') {
           this.imageTypeDescription = "ImageId Input";
@@ -58,17 +65,43 @@ export class AwsvmComponent implements OnInit {
     }
 
     this.imageService.imageLoadImages(imageRequest).subscribe(
-
-      types => {
-        this.images  = types.images;
+      images => {
+        this.images  = images.images;
       },
       error => {
         console.log(error);
       });
   }
 
-  increaseBy(value) {
-    this.displayNum += value;
+  loadInstanceTypes() {
+    this.typesSearchInfo = true;
+
+    if(this.f.maxMemory.value) {
+      this.instancetypeService.instancetypeLoadInstanceTypeWithMemory(this.f.maxMemory.value).subscribe(
+        types => {
+          this.types  = types.types;
+        },
+        error => {
+          console.log(error);
+        });
+    }else {
+      this.instancetypeService.instancetypeLoadInstanceType().subscribe(
+        types => {
+          this.types  = types.types;
+        },
+        error => {
+          console.log(error);
+        });
+    }
+
+  }
+
+  increaseImagesBy(value) {
+    this.displayNumImages += value;
+  }
+
+  increaseTypesBy(value) {
+    this.displayNumTypes += value;
   }
 
   get f() { return this.newVMForm.controls; }
