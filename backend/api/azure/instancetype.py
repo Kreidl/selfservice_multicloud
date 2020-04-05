7b52a1b0130187dc6417b4074b12fb2b0bc81f0a
Kreidl <lukaskienast0@gmail.com>
2020-04-05 17:34:50 +0200

    Implemented azure instancetype in api

--- backend/api/azure/instancetype.py
+++ backend/api/azure/instancetype.py
@@ -6,3 +6,13 @@ import configparser
 config = configparser.ConfigParser()
 config.read_file(open(r'config.txt'))
 typeURL = config.get('MicroServiceURL', 'azure-instancetype')
+
+
+#Get all Instancetypes
+def getAllTypes():
+    try:
+        publishers = json.loads(requests.get(typeURL + '/types').text)
+    except Exception:
+        return make_response(jsonify(None))
+
+    return make_response(jsonify(publishers))
